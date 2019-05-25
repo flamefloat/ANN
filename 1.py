02bab2ee9c6d5a38594334f5db70636679387016
e <minghao@pku.edu.cn>
2019-05-25 21:16:13 +0800

    BP计算完成

--- 1.py
+++ 1.py
@@ -1,12 +1,7 @@
 import numpy as np 
 a = np.array([[1,2,3],[4,5,6]])
-b = np.array([1,0])
-
-
-
-print(b.shape)
-b = b.reshape(1,2)
-print(b.shape)
-c = b.T
-print(np.exp(-c))
-
+b = np.array([[1,2,3]])
+aa = np.dot(a,b.T)
+bb = np.dot(b,a.T)
+print(aa)
+print(bb.T)
